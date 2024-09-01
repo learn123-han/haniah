4a5c4e78daf0894680a6f78603452e4360aac0bc
learn123-han <haniahfatima@gmail.com>
2024-09-01 12:31:46 +0530

    Add files via upload
    Special Minds is a user friendly mobile app that recognize early symptoms of learning disabilities.It is accessible to everyone allowing parents , teachers to login and select from various quiz topics related to autism, low vision etc.It provides a timed questionnaire and immediate scoring.

---
+++ QuizActivity.kt
@@ -0,0 +1,153 @@
+package com.example.specialminds
+
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.specialminds.databinding.ActivityQuizBinding
+import com.example.specialminds.databinding.ScoreDialogBinding
+import kotlin.math.min
+
+class QuizActivity : AppCompatActivity(),View.OnClickListener {
+
+    companion object {
+        var questionModelList : List<QuestionModel> = listOf()
+        var time : String = ""
+    }
+
+    lateinit var binding: ActivityQuizBinding
+
+    var currentQuestionIndex = 0;
+    var selectedAnswer = ""
+    var score = 0;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityQuizBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            btn0.setOnClickListener(this@QuizActivity)
+            btn1.setOnClickListener(this@QuizActivity)
+            btn2.setOnClickListener(this@QuizActivity)
+
+            nextBtn.setOnClickListener(this@QuizActivity)
+        }
+
+        loadQuestions()
+        startTimer()
+    }
+
+    private fun startTimer(){
+        val totalTimeInMillis = time.toInt()* 60*1000L
+        object : CountDownTimer(totalTimeInMillis,1000L){
+            override fun onTick(millisUntilFinished: Long) {
+                val seconds = millisUntilFinished /1000
+                val minutes = seconds/60
+                val remainingSeconds = seconds% 60
+                binding.timeIndicatorTextview.text = String.format("%02d:%02d", minutes,remainingSeconds)
+
+            }
+
+            override fun onFinish() {
+                //Finish the quiz
+            }
+
+        }.start()
+    }
+
+    private fun loadQuestions(){
+        selectedAnswer = ""
+        if(currentQuestionIndex == questionModelList.size){
+            finishQuiz()
+            return
+        }
+
+        binding.apply {
+            questionIndicatorTextview.text = "Question ${currentQuestionIndex+1}/ ${questionModelList.size} "
+            questionProgressIndicator.progress =
+                ( currentQuestionIndex.toFloat() / questionModelList.size.toFloat() * 100 ).toInt()
+            questionTextview.text = questionModelList[currentQuestionIndex].question
+            btn0.text = questionModelList[currentQuestionIndex].options[0]
+            btn1.text = questionModelList[currentQuestionIndex].options[1]
+            btn2.text = questionModelList[currentQuestionIndex].options[2]
+
+        }
+    }
+
+    override fun onClick(view: View?) {
+
+        binding.apply {
+            btn0.setBackgroundColor(getColor(R.color.gray))
+            btn1.setBackgroundColor(getColor(R.color.gray))
+            btn2.setBackgroundColor(getColor(R.color.gray))
+
+
+            val clickedBtn = view as Button
+            if (clickedBtn.id == R.id.next_btn) {
+                //next button is clicked
+                if (selectedAnswer.isEmpty()) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Please select answer to continue",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return;
+                }
+                if (selectedAnswer == questionModelList[currentQuestionIndex].correct) {
+                    score++
+                    Log.i("Score of quiz", score.toString())
+                }
+                currentQuestionIndex++
+                loadQuestions()
+            } else {
+                //options button is clicked
+                selectedAnswer = clickedBtn.text.toString()
+                clickedBtn.setBackgroundColor(getColor(R.color.pea))
+            }
+        }
+    }
+
+     private fun finishQuiz(){
+        val totalQuestions = questionModelList.size
+        val percentage = ((score.toFloat() / totalQuestions.toFloat() ) *100 ).toInt()
+
+        val dialogBinding  = ScoreDialogBinding.inflate(layoutInflater)
+        dialogBinding.apply {
+            scoreProgressIndicator.progress = percentage
+            scoreProgressText.text = "$percentage %"
+            if(percentage>50){
+                scoreTitle.text = "Everything is FINE!!"
+                scoreTitle.setTextColor(Color.GREEN)
+            }
+            else if (percentage>25){
+                scoreTitle.text = "Child is Average"
+                scoreTitle.setTextColor(Color.BLUE)
+
+            }
+            else
+            {
+                scoreTitle.text = "Child needs special care"
+                scoreTitle.setTextColor(Color.RED)
+            }
+
+
+
+            scoreSubtitle.text = "$score out of $totalQuestions are correct"
+            finishBtn.setOnClickListener {
+                finish()
+            }
+        }
+
+        AlertDialog.Builder(this)
+            .setView(dialogBinding.root)
+            .setCancelable(false)
+            .show()
+
+    }
+}
